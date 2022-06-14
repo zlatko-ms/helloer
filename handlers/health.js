677bf9cdb357fd0311332c3599f0a3e7bac9ee35
zlatko-ms <azlatko@gmail.Com>
2022-06-14 14:51:44 +0200

    added flag to remove probe hits from the logs, as they can be a little bit overheling

--- handlers/health.js
+++ handlers/health.js
@@ -2,12 +2,15 @@
 
 const { Logger } = require('@util/logger.js');
 var { responseBuilder }  = require('@util/response.js');
+const { logProbeCalls } = require('@util/configuration.js')
 
 module.exports.healthProbeHandler = function(req,res) {
     Logger.info("recv probe request");
     var responsePayload = responseBuilder(req);
     responsePayload['status']="healthy";
     res.json(responsePayload);
-    Logger.info("sent probe response to client from "+responsePayload['request-source-ip']);
+    if (logProbeCalls) {
+        Logger.info("sent probe response to client from "+responsePayload['request-source-ip']);
+    }
 }
 
--- util/configuration.js
+++ util/configuration.js
@@ -5,10 +5,12 @@ const confBackendId = process.env.HELLOER_BACKEND_ID || uuidv4();
 const confAppPort = process.env.HELLOER_PORT || 8080;
 const confUrlSpoke = process.env.HELLOER_FORWARD_PRIVATE_URL || "http://localhost:8080/connectivity/local";
 const confUrlOnPrem = process.env.HELLOER_FORWARD_ONPREM_URL || "http://my.host.is:9086/whatever";
+const confLogProbes = process.env.HELLOER_LOG_PROBES || "false"
 
 module.exports.backendType = confBackendType;
 module.exports.backendId = confBackendId;
 module.exports.appPort = confAppPort;
 module.exports.forwardSpokeUrl = confUrlSpoke;
 module.exports.forwardOnPremUrl = confUrlOnPrem;
+module.exports.logProbeCalls = Boolean(confLogProbes)
 
