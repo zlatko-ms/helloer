eff9cb7f107f2cf6f1820d068a20a2559b2a76f0
zlatko-ms <azlatko@gmail.Com>
2022-08-08 16:40:08 +0200

    - re-structed source directory to separate app from test code - added basic test on the response handler

---
+++ test/response.js
@@ -0,0 +1,42 @@
+
+require('module-alias/register')
+
+var expect = require("chai").expect;
+var { responseBuilder }  = require('@util/response.js');
+var httpMocks = require('node-mocks-http');
+
+const localhost = "127.0.0.1"
+const unknown = "unknown"
+
+describe("Response Builder", function() {
+    describe("Source Address Fetching", function() {
+      it("returns 'unknown' if source address cannot be fetched", function() {
+        var request  = httpMocks.createRequest({});
+        var responsePayload = responseBuilder(request);
+        expect(responsePayload.request_source_ip).to.equal(unknown)
+      });
+      it("fetches the source address from request header map", function() {
+        var request  = httpMocks.createRequest({
+            headers : {
+                'x-forwarded-for' : localhost
+            }
+        });
+        var responsePayload = responseBuilder(request);
+        expect(responsePayload.request_source_ip).to.equal(localhost)
+
+      });
+      it("feches the source address from the request socket map", function()  {
+        var request  = httpMocks.createRequest({
+           
+            socket : {
+                'remoteAddress' : localhost
+            }
+        });
+        var responsePayload = responseBuilder(request);
+        expect(responsePayload.request_source_ip).to.equal(localhost)
+      });
+      
+    });
+  
+
+  });
